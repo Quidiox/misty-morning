c08f17bc14d1e8272d5e653d9b25ff999671ddd0
Quidiox <no email>
2019-10-29 09:47:55 +0200

    demoing lint-staged

--- src/components/Frontpage.vue
+++ src/components/Frontpage.vue
@@ -2,9 +2,10 @@
   <v-container>
     <h1>Welcome to Misty Morning!</h1>
     <br />
-    <p>To create new misty mornings you need to have an user account. If you already have an user account you can <v-btn text>login</v-btn> or you can <v-btn text>register</v-btn></p>
+    <p>To create new misty mornings you need to have an user account.</p>
     <p>To mark your availability to misty morning someone else has created you need to have the url of the misty morning.</p>
-  </v-container>
+    <p><v-btn text>login</v-btn> or <v-btn text>register</v-btn></p>
+         </v-container>
 </template>
 
 <script>
